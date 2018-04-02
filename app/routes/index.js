8dee96b990fee219b67e571e15657e734821540b
Ryan Hafner <haf@Ryans-MacBook-Pro.local>
2018-04-01 23:54:04 -0400

    added cron routes

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,8 +1,11 @@
 const botRoutes = require('./bot_routes'),
       sensorRoutes = require('./sensor_routes'),
-      sequenceRoutes = require('./sequence_routes');
+      sequenceRoutes = require('./sequence_routes'),
+      cronRoutes = require('./cron_routes');
+
 module.exports = function(app, db) {
     botRoutes(app, db);
     sequenceRoutes(app, db);
     sensorRoutes(app, db);
+    cronRoutes(app, db);
 };
